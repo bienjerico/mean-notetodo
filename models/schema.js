6fae7cd61570c50da162084aa0c75c95a2b0cc22
Bien Jerico Cueto <bien.jerico@gmail.com>
2016-07-23 11:05:07 +0800

    add date in note

--- models/schema.js
+++ models/schema.js
@@ -2,12 +2,13 @@ var mongoose = require('mongoose');
 
 var NoteSchema = new mongoose.Schema({
   title : { type: String, require: true},
-  text : String
+  text : { type: String, require: true},
+  date: { type: Date, default: Date.now }
 });
 var Notes = mongoose.model('Notes',NoteSchema);
 
 var TodoSchema = new mongoose.Schema({
-  entry : String,
+  entry : { type: String, require: true},
   checkbox: {
         type: Boolean,
         default: false
